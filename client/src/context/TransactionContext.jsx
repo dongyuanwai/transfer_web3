40db68b4cd5009a51e92e058787a2e4538c96a0a
dongyuanwai <yuanwaidong@gmail.com>
2023-07-30 22:43:33 +0800

    feat: 新增联系方式

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -27,9 +27,9 @@ export const TransactionProvider = ({ children }) => {
 	const [currentAccount, setCurrentAccount] = useState("")
 	const [formData, setformData] = useState({ addressTo: "", amount: "", keyword: "", message: "" });
 	const [isLoading, setIsLoading] = useState(false);
-  const [transactionCount, setTransactionCount] = useState(localStorage.getItem("transactionCount"));
-  const [transactions, setTransactions] = useState([]);
-	
+	const [transactionCount, setTransactionCount] = useState(localStorage.getItem("transactionCount"));
+	const [transactions, setTransactions] = useState([]);
+
 	const handleChange = (e, name) => {
 		setformData((prevState) => ({ ...prevState, [name]: e.target.value }));
 	}
@@ -54,45 +54,45 @@ export const TransactionProvider = ({ children }) => {
 	}
 
 	const checkIfTransactionsExists = async () => {
-    try {
-      if (ethereum) {
-        const transactionsContract = getEthereumContract();
-        const currentTransactionCount = await transactionsContract.getTransactionCount();
+		try {
+			if (ethereum) {
+				const transactionsContract = getEthereumContract();
+				const currentTransactionCount = await transactionsContract.getTransactionCount();
 
-        window.localStorage.setItem("transactionCount", currentTransactionCount);
-      }
-    } catch (error) {
-      console.log(error);
+				window.localStorage.setItem("transactionCount", currentTransactionCount);
+			}
+		} catch (error) {
+			console.log(error);
 			throw new Error("No ethereum object");
-    }
-  };
+		}
+	};
 
 	const getAllTransactions = async () => {
-    try {
-      if (ethereum) {
-        const transactionsContract = getEthereumContract();
-        const availableTransactions = await transactionsContract.getAllTransactions();
+		try {
+			if (ethereum) {
+				const transactionsContract = getEthereumContract();
+				const availableTransactions = await transactionsContract.getAllTransactions();
 
-        const structuredTransactions = availableTransactions.map((transaction) => ({
-          addressTo: transaction.receiver,
-          addressFrom: transaction.sender,
-          timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
-          message: transaction.message,
-          keyword: transaction.keyword,
-          amount: parseInt(transaction.amount._hex) / (10 ** 18)
-        }));
+				const structuredTransactions = availableTransactions.map((transaction) => ({
+					addressTo: transaction.receiver,
+					addressFrom: transaction.sender,
+					timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
+					message: transaction.message,
+					keyword: transaction.keyword,
+					amount: parseInt(transaction.amount._hex) / (10 ** 18)
+				}));
 
-        console.log(structuredTransactions);
+				console.log(structuredTransactions);
 
-        setTransactions(structuredTransactions);
-      } else {
-        if (!ethereum) return alert("请安装Metamask!");
-      }
-    } catch (error) {
-      console.log(error);
+				setTransactions(structuredTransactions);
+			} else {
+				if (!ethereum) return alert("请安装Metamask!");
+			}
+		} catch (error) {
+			console.log(error);
 			throw new Error("没有获取到ethereum对象")
-    }
-  };
+		}
+	};
 
 	// 连接钱包
 	const connectWallet = async () => {
@@ -100,6 +100,7 @@ export const TransactionProvider = ({ children }) => {
 			if (!ethereum) return alert("请安装Metamask!");
 			const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 			setCurrentAccount(accounts[0])
+			window.location.reload();
 		} catch (error) {
 			console.log(error)
 			throw new Error("没有获取到ethereum对象")
